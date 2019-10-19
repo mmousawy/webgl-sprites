6f1d16c89495ad0653b3b4f87e6aeb96637c197a
mmousawy <murtada.al.mousawy@gmail.com>
2019-10-19 14:29:50 +0200

    Add animated belt plane and grid

--- script.js
+++ script.js
@@ -1,24 +1,63 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+const planes = [];
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-camera.position.z = 5;
+var size = 10000;
+var divisions = 10000;
 
-var animate = function () {
-  requestAnimationFrame( animate );
+var gridHelper = new THREE.GridHelper( size, divisions );
+gridHelper.rotation.x = THREE.Math.degToRad(90);
+gridHelper.position.x = 1;
+gridHelper.position.y = 0;
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+scene.add(gridHelper);
 
-  renderer.render( scene, camera );
+const texture = new THREE.TextureLoader().load("assets/belt.png");
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.minFilter = THREE.NearestFilter;
+texture.magFilter = THREE.NearestFilter;
+
+const geometry = new THREE.PlaneGeometry(1,1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0x999999,
+  side: THREE.DoubleSide,
+  map: texture
+});
+
+const plane = new THREE.Mesh(geometry, material);
+
+plane.position.x = .5;
+plane.position.y = .5;
+plane.texture = texture;
+plane.speed = 2;
+plane.currentFrame = 0;
+plane.rotation.z = THREE.Math.degToRad(-90);
+
+planes.push(plane);
+
+scene.add(plane);
+
+camera.position.z = 10;
+
+const animate = function () {
+  requestAnimationFrame(animate);
+
+  planes.forEach(plane => {
+    plane.currentFrame++;
+
+    if (plane.currentFrame === plane.speed) {
+      plane.texture.offset.x -= 0.0625;
+      plane.currentFrame = 0;
+    }
+  });
+
+  renderer.render(scene, camera);
 };
 
 animate();
